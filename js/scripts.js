43473ede9db3b32cc3fbb3c74e196a5ba0b37fbc
bart@aseenrichment.com <Guest@Epicodus-9G.local>
2017-07-07 10:38:10 -0700

    add submit button code into scripts.js to show survey results of firstName and lastName

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,39 @@
 $(document).ready(function() {
-  $("#formAddress").submit(function(){
+  $("#formSurvey").submit(function(){
     var firstNameInput = $("input#firstName").val();
     var lastNameInput = $("input#lastName").val();
-    var addressInput = $("input#address").val();
-    var phoneNumberInput = $("input#phoneNumber").val();
-    var emailAddressInput = $("input#emailAddress").val();
+    // var trackInput = $("input:radio[name=track]:checked").val();
+    // var trackScore = parseInt("trackInput").val();
 
     $(".firstName").text(firstNameInput);
     $(".lastName").text(lastNameInput);
-    $(".address").text(addressInput);
-    $(".phoneNumber").text(phoneNumberInput);
-    $(".emailAddress").text(emailAddressInput);
-    $("#contact").show();
-    // $(".panel-body").hide();
-    $("#addressRow").prepend('<div class ="panel panel-success"> <div class = "panel-heading">' + firstNameInput + lastNameInput + '</div><div class="panel-body">' + addressInput + phoneNumberInput + emailAddressInput + '</div>');
 
-    $(".panel-success").first().click(function(){
-      $(this).find(".panel-body").toggle();
-      // $(this).find(".panel-heading").toggle();
-    });
-    // $("ul#user").prepend("<li>Hello!</li>");
+    // $(".track").text(trackInput);
+    $("#survey").show();
+
+
+
+
+
+
+
+
+
+
+    // if (trackinput === 1){
+    //   $("#webdesigner").show();
+    // }else if (trackScore === 2){
+    //   $("#androidapp").show();
+    // }else (trackScore === 3){
+    //   $("#fullstack").show();
+    // }
+
+
+
+    // $(".panel-success").first().click(function(){
+    //   $(this).find(".panel-body").toggle();
+    // });
+
     event.preventDefault();
   });
 
